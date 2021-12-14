627d03186eb93e9675800c71416575bbbd97dfe4
Lachlan Marnham <lachlan.marnham@farfetch.com>
2021-12-14 13:23:35 +0000

    Add setWorkingDirectoryToGitRoot

--- src/modules/git.js
+++ src/modules/git.js
@@ -2,28 +2,35 @@
 import git from 'isomorphic-git';
 import fs from 'fs';
 import {globby} from 'globby';
+import path from 'path';
 
 async function gitStatus() {
     let status = await git.status({ fs, dir: '.', filepath: 'README.md' });
     console.log(status);
 }
 
+async function getGitRoot() {
+    // Finds the project root by identifying the parent directory of .git
+    return git.findRoot({fs, filepath: process.cwd()});
+}
+
+async function setWorkingDirectoryToGitRoot() {
+    const gitRoot = await getGitRoot();
+    process.chdir(gitRoot);
+}
+
 async function getGitFiles() {
     // Gets the list of all files which aren't ignored (ie by the .gitignore).
     // This includes staged or unstaged, new files and deleted files.
-    return globby(['./**', './**/.*'], { gitignore: true });
-}
-
-async function getGitRoot() {
-    // Finds the project root by identifying the parent directory of .git
-    return git.findRoot({fs, filepath: process.cwd()})
+    const gitRoot = await getGitRoot();
+    var visibleFilesMatch = path.join(gitRoot, './**');
+    var hiddenFilesMatch = path.join(gitRoot, './**/.*');
+    return globby([visibleFilesMatch, hiddenFilesMatch], { gitignore: true });
 }
 
 async function isClean() {
     const paths = await getGitFiles();
     console.log(paths);
-    const gitRoot = await getGitRoot();
-    console.log(gitRoot);
     // for (const filepath of paths) {
     //     await git.add({ fs, dir, filepath });
     // }
