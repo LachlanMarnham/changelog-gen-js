5ecaf4a1fa2dbccf73bad8e0168e6ea35cbe7a3e
Lachlan Marnham <lachlan.marnham@farfetch.com>
2021-12-14 15:36:57 +0000

    add src/dummy.js

---
+++ src/dummy.js
@@ -0,0 +1,23 @@
+'use strict';
+import git from 'isomorphic-git';
+import fs from 'fs';
+
+async function main(){
+    // set the working directory to the git root
+    const gitRoot = await git.findRoot({fs, filepath: process.cwd()});
+    process.chdir(gitRoot);
+
+    // get the files git knows about
+    let files = await git.listFiles({ fs, dir: '.' });
+
+    // print 'working tree not clean' if there are any files which don't have 
+    // the status: 'unmodified'
+    for (const filepath of files) {
+        let status = await git.status({ fs, dir: '.', filepath: filepath });
+        if (status !== 'unmodified') {
+            console.log('working tree not clean');
+        };
+    }
+}
+
+await main();
