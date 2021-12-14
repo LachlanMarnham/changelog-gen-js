9746ca20829c50ab186fac292fe11537df798168
Lachlan Marnham <lachlan.marnham@farfetch.com>
2021-12-14 01:18:40 +0000

    Add getGitFiles

--- src/modules/git.js
+++ src/modules/git.js
@@ -1,15 +1,25 @@
 'use strict';
 import git from 'isomorphic-git';
 import fs from 'fs';
+import {globby} from 'globby';
 
 async function gitStatus() {
     let status = await git.status({ fs, dir: '.', filepath: 'README.md' });
     console.log(status);
 }
 
+async function getGitFiles() {
+    // Gets the list of all files which aren't ignored (ie by the .gitignore).
+    // This includes staged or unstaged, new files and deleted files.
+    return globby(['./**', './**/.*'], { gitignore: true });
+}
+
 async function isClean() {
-    let files = await git.listFiles({ fs, dir: '.' });
-    console.log(files);
+    const paths = await getGitFiles();
+    console.log(paths);
+    // for (const filepath of paths) {
+    //     await git.add({ fs, dir, filepath });
+    // }
 }
 
 export { gitStatus, isClean };
