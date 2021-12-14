6fa26e9e133d5b0d549eb92c92ac8e8b9b4eb142
Lachlan Marnham <lachlan.marnham@farfetch.com>
2021-12-14 18:42:35 +0000

    Add execShellCommand to avoid callbacks

--- src/modules/vcs/git.js
+++ src/modules/vcs/git.js
@@ -1,19 +1,32 @@
 import { exec } from "child_process";
 
-
-function describe() {
-    exec("git describe --tags --dirty --long --match [0-9]*", (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(stdout.trim().split("-"));
-        return stdout;
+function execShellCommand(cmd) {
+    return new Promise((resolve, reject) => {
+     exec(cmd, (error, stdout, stderr) => {
+      if (error) {
+       console.warn(error);
+      }
+      resolve(stdout? stdout : stderr);
+     });
     });
+   }
+
+async function describe() {
+    // exec("git describe --tags --dirty --long --match [0-9]*", (error, stdout, stderr) => {
+    //     if (error) {
+    //         console.log(`error: ${error.message}`);
+    //         return;
+    //     }
+    //     if (stderr) {
+    //         console.log(`stderr: ${stderr}`);
+    //         return;
+    //     }
+    //     console.log(stdout.trim().split("-"));
+    //     return stdout;
+    // });
+    let result = await execShellCommand("git describe --tags --dirty --long --match [0-9]*");
+    result = result.trim().split("-");
+    console.log(result);
 }
 
-describe();
+await describe();
