404102a191574cd141ab17321933fcd6c6113814
Lachlan Marnham <lachlan.marnham@farfetch.com>
2021-12-14 15:14:42 +0000

    Return files from getGitFiles and getFilesOnDisk as Sets

--- src/modules/git.js
+++ src/modules/git.js
@@ -2,7 +2,7 @@
 import git from 'isomorphic-git';
 import fs from 'fs';
 import {globby} from 'globby';
-import path from 'path';
+import {setUnion} from './utils.js';
 
 async function gitStatus(filepath) {
     return git.status({ fs, dir: '.', filepath: filepath });
@@ -21,22 +21,27 @@ async function setWorkingDirectoryToGitRoot() {
 }
 
 async function getFilesOnDisk() {
-    // Gets the list of all files which aren't ignored (ie by the .gitignore).
+    // Gets the Set of all files which aren't ignored (ie by the .gitignore).
     // This does not include files which have been deleted
     let visibleFilesMatch =  './**';
     let hiddenFilesMatch = './**/.*';
-    return globby([visibleFilesMatch, hiddenFilesMatch], { gitignore: true });
+    let filesList = await globby([visibleFilesMatch, hiddenFilesMatch], { gitignore: true });
+    return new Set(filesList);
 }
 
 async function getGitFiles() {
-    // Gets the list of all files which aren't ignored (ie by the .gitignore).
+    // Gets the Set of all files which aren't ignored (ie by the .gitignore).
     // This includes staged or unstaged, new files and deleted files. It does not include
     // files which are both new and unstaged
-    return git.listFiles({ fs, dir: '.' });
+    let filesList = await git.listFiles({ fs, dir: '.' });
+    return new Set(filesList);
 }
 
 async function isClean() {
-    const paths = await getFilesOnDisk();
+    const pathsOnDisk = await getFilesOnDisk();
+    const gitPaths = await getGitFiles();
+    const paths = setUnion(pathsOnDisk, gitPaths);
+    
     for (const filepath of paths) {
         let status = await gitStatus(filepath);
         if (status !== 'unmodified') {
